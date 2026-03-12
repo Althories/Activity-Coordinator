433ec28309be166e95ac52b9d6f7b84f3ed78d68
cooperptacek <cooperptacek@gmail.com>
2026-03-12 16:24:00 -0500

    Temporary Hello Kitty Page
    Made landing page that will let you switch between the profile (TRUE) and the filter (FALSE). For now tru just brings you to the landing page as the profile isnt done

--- app/src/main/java/com/bignerdranch/android/activity_coordinator/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/activity_coordinator/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.activity_coordinator
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_filter)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.filter)) { v, insets ->
+        enableEdgeToEdge() // allows app to go behind system bars
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets -> // for edge to egde
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+// when you want to reference a class itself (not an instance of it) you use ::class. But startActivity and Intent are Java-based Android APIs, so they expect a Java-style class reference.
+        findViewById<Button>(R.id.true_button).setOnClickListener {
+            startActivity(Intent(this, ProfileActivity::class.java))
+        }
+
+        findViewById<Button>(R.id.false_button).setOnClickListener {
+            startActivity(Intent(this, FilterActivity::class.java))
         }
     }
 }
