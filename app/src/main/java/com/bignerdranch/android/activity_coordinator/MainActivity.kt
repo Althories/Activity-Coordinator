092f098bb56399fd3ea1e06892438bdf762303b5
cooperptacek <cooperptacek@gmail.com>
2026-03-17 23:33:38 -0500

    Revert "Merge pull request #1 from Althories/f-filter_functionality"
    This reverts commit 836dbe1a50409f22befe88c4d378a29e06a8df8a, reversing
    changes made to bab94904c354507caa7fae0ab5b3407b0cf2c507.

--- app/src/main/java/com/bignerdranch/android/activity_coordinator/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/activity_coordinator/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.bignerdranch.android.activity_coordinator
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,20 +9,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge() // allows app to go behind system bars
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets -> // for edge to egde
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-// when you want to reference a class itself (not an instance of it) you use ::class. But startActivity and Intent are Java-based Android APIs, so they expect a Java-style class reference.
-        findViewById<Button>(R.id.profile_button).setOnClickListener {
-            startActivity(Intent(this, ProfileActivity::class.java))
-        }
-
-        findViewById<Button>(R.id.filter_button).setOnClickListener {
-            startActivity(Intent(this, FilterActivity::class.java))
         }
     }
 }
