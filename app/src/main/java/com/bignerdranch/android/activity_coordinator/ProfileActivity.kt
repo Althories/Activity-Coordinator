6d2c77b3d38c9431d78bde218db318db178a6318
Reedspun <reedspun@gmail.com>
2026-04-06 17:00:21 -0500

    Rewrote profile pic updating
    Profile pictures now are stored in a mutable map in usersession, rather than a single variable. This will allow me to store all loaded profile pictures in one place.
    Also fixed BMCP's profile picture not showing up because it was somehow too big by hardcoding the max profile picture download size to about 100kb.

--- app/src/main/java/com/bignerdranch/android/activity_coordinator/ProfileActivity.kt
+++ app/src/main/java/com/bignerdranch/android/activity_coordinator/ProfileActivity.kt
@@ -92,9 +92,9 @@ class ProfileActivity : AppCompatActivity() {
         val pfp = findViewById<ImageView>(R.id.pfp) // Profile picture view
         // The user's profile picture is stored as a jpg named their uid.
         val pfpRef = UserSession.storage.reference.child(
-            "userProfilePictures/$currentUserId.jpg")
+            "userProfilePictures/$uid.jpg")
         // current profile picture will be stored as a bitmap in usersession
-        pfp.setImageBitmap(UserSession.pfp)
+        pfp.setImageBitmap(UserSession.getPfp())
 
         var changedPfp = false // Flag for whether we should upload the profile pic when saving
         // ^ Branden
@@ -153,7 +153,7 @@ class ProfileActivity : AppCompatActivity() {
                         Log.w(TAG, "Upload failed", e)
                     }.addOnSuccessListener { taskSnapshot ->
                         Log.d(TAG, "Upload successful: $taskSnapshot")
-                        UserSession.pfp = bitmapScaled // Update the current session's profile picture
+                        UserSession.updatePfpLocally(bitmapScaled) // Update the current session's profile picture
                     }
 
                     changedPfp = false // reset flag
